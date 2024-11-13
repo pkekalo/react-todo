0105a06cea65a4a8cd9b24507b421517eaf54df2
Petr Kekalo <kekalo.petr@gmail.com>
2024-11-13 04:07:55 -0500

    Lists in React are added

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,32 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const todoList = [
+  {
+    id: 1,
+    title: 'Breakfast'
+  },
+  {
+    id: 2,
+    title: 'Shower'
+  },
+  {
+    id: 3,
+    title: 'Drive'
+  }
+];
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        <h1>Todo List</h1>
+        <ul>
+          {todoList.map(item => (
+            <li key={item.id}>{item.title}</li>
+          ))}
+        </ul>
+    </> 
   )
 }
 
