2aa0cd5bf9efe3ae768d5c056be4c876d55f42ab
Petr Kekalo <kekalo.petr@gmail.com>
2024-11-19 23:09:43 -0500

    Moved List Item into New Todo List Item Component, Handled Add Todo Form Submit, Stored New Todo in React State

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,31 @@ import AddTodoForm from './AddTodoForm';
 
 function App() {
 
+  const [todos, setTodos] = useState([
+    { id: 1, title: 'Breakfast' },
+    { id: 2, title: 'Shower' },
+    { id: 3, title: 'Drive' },
+  ]);
+
+  const [newTodo, setNewTodo] = useState('');
+
+  const addTodo = (title) => {
+    setNewTodo(title);
+    
+    // setTodos(previousState => {
+    //   return [...previousState, { id: previousState.length + 1, title }];
+    // })
+    
+    setTodos([...todos, { id: todos.length + 1, title }]);
+  };
+
   return (
     <>
       <div>
           <h1>Todo List</h1>
-          <AddTodoForm />
-          <TodoList />
+          <AddTodoForm onAddTodo={addTodo} />
+          <p>New Todo: {newTodo}</p>
+          <TodoList todos={todos} />
       </div>
     </> 
   )
