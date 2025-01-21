591e0685a66daad421a1c65b709d23309163a6a3
Petr Kekalo <kekalo.petr@gmail.com>
2025-01-21 13:16:32 -0500

    Add loading state and asynchronous data fetching with conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
-import { useSemiPersistentState } from './useSemiPersistentState';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [todoList, setTodoList] = useSemiPersistentState();
-  
+  const [todoList, setTodoList] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = () => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve({ data: { todoList: [{ id: 1, text: 'Example Todo' }] } });
+        }, 2000); // 2 seconds wait
+      });
+    };
+
+    fetchData().then((result) => {
+      setTodoList(result.data.todoList);
+      setIsLoading(false); 
+    });
+  }, []);
+
+  useEffect(() => {
+    if(!isLoading) {
+      localStorage.setItem("savedTodoList", JSON.stringify(todoList)); 
+    }
+    
+  }, [todoList, isLoading]);
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]); 
@@ -18,8 +41,14 @@ function App() {
   return (
     <>
       <h1>Todo List</h1>
-      <AddTodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>
+      {isLoading ? (
+        <p>Loading...</p> 
+      ) : (
+      <>
+        <AddTodoForm onAddTodo={addTodo} />
+        <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+      </>
+      )}
     </>
   );
 }
