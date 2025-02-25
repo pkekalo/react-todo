7c53b55f946400f71b3b9ae317fd6eef790ecfa8
Petr Kekalo <kekalo.petr@gmail.com>
2025-02-25 18:06:37 -0500

    Implemented sorting by Airtable view to ensure list items appear in correct order

--- src/App.jsx
+++ src/App.jsx
@@ -13,28 +13,28 @@ function App() {
   useEffect(() => {
 
     const fetchData = async () => {
-      const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+      const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}?view=Grid%20view`;
       const options = {
         method: "GET",
         headers: { Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}` },
       };
-
+    
       try {
         const response = await fetch(url, options);
         if (!response.ok) throw new Error(`Error: ${response.status}`);
-
+    
         const data = await response.json();
         const todos = data.records.map((todo) => ({
           id: todo.id,
           title: todo.fields.title,
         }));
-
+    
         setTodoList(todos);
         setIsLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error.message);
       }
-    };
+    };    
 
     fetchData();
   }, []); 
